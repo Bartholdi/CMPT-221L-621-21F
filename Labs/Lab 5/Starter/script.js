f8678723a74eaf7c5741c32f4997647c9c7b4e6d
Bartholdi <79282610+Bartholdi@users.noreply.github.com>
2021-10-06 20:28:39 -0400

    Lab

---
+++ Labs/Lab 5/Starter/script.js
@@ -0,0 +1,8 @@
+document.getElementById("title").innerHTML = "Hello From Joe Bartholdi";
+document.getElementById("bossy").innerHTML = "Be Nice Please";
+document.getElementById("description").innerHTML = "Joe Bartholdi";
+document.getElementById("blue").style.color = "blue";
+document.getElementById("cream").style.backgroundColor = "#FFFDD0";
+document.body.style.backgroundColor = "grey";
+document.getElementById("caps").style.textTransform = "uppercase";
+document.getElementById("addTitle").classList.add("title");
